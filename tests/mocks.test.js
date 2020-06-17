a079a018365172f39a74cb5a76a9b043251fa012
Isaiah Ritter <isaiah@casted.us>
2020-06-17 17:12:26 -0400

    finishing up mock tests

--- tests/mocks.test.js
+++ tests/mocks.test.js
@@ -1,3 +1,6 @@
+// More on mock calls and Jest at:
+// https://jestjs.io/docs/en/mock-functions
+
 function forEach(items, callback) {
   for (let index = 0; index < items.length; index++) {
     callback(items[index]);
@@ -19,4 +22,59 @@ test("testing mock callback", () => {
 
   // The return value of the first call to the function was 42
   expect(mockCallback.mock.results[0].value).toBe(42);
+});
+
+test("second mock callback test", () => {
+  const myMock = jest.fn();
+
+  const a = new myMock();
+  const b = {};
+  const bound = myMock.bind(b);
+  bound();
+
+  // console.log(myMock.mock.instances);
+
+  //  The function was called exactly once
+  // expect(someMockFunction.mock.calls.length).toBe(1);
+
+  // The first arg of the first call to the function was 'first arg'
+  // expect(someMockFunction.mock.calls[0][0]).toBe("first arg");
+
+  // The second arg of the first call to the function was 'second arg'
+  // expect(someMockFunction.mock.calls[0][1]).toBe("second arg");
+
+  // The return value of the first call to the function was 'return value'
+  // expect(someMockFunction.mock.results[0].value).toBe("return value");
+
+  // This function was instantiated exactly twice
+  // expect(someMockFunction.mock.instances.length).toBe(2);
+
+  // The object returned by the first instantiation of this function
+  // had a `name` property whose value was set to 'test'
+  // expect(someMockFunction.mock.instances[0].name).toEqual("test");
+
+  // NOTE: this is a filler to pass the test
+  expect(true).toBeTruthy();
+});
+
+test("third mock callback test", () => {
+  const myMock = jest.fn();
+  console.log(myMock());
+
+  myMock.mockReturnValueOnce(10).mockReturnValueOnce("x").mockReturnValue(true);
+
+  console.log(myMock(), myMock(), myMock(), myMock());
+});
+
+test("fourth mock callback test", () => {
+  const filterTestFn = jest.fn();
+
+  // Make the mock return 'true' for the first call,
+  // and 'false' for the second call
+  filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+
+  const result = [11, 12].filter((num) => filterTestFn(num));
+
+  console.log(result);
+  console.log(filterTestFn.mock.calls);
 });
