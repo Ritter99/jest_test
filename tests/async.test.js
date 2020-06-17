fce5caeafd7840d2d32171c6e5524c8b80455e42
Isaiah Ritter <isaiah@casted.us>
2020-06-17 16:17:36 -0400

    updating all files

--- tests/async.test.js
+++ tests/async.test.js
@@ -7,20 +7,22 @@
 //   fetchData(callback);
 // });
 
-test('the data is peanut butter', () => {
+const fetchData = require("node-fetch");
+
+test("the data is peanut butter", () => {
   function callback() {
     try {
-      expect(data).toBe('peanut butter')
+      expect(data).toBe("peanut butter");
     } catch (error) {
-      done(error)
+      done(error);
     }
   }
 
-  fetchData(callback)
-})
+  fetchData(callback);
+});
 
-test('the data is peanut butter', () => {
-  return fetchData().then(data => {
-    expect(data).toBe('peanut butter')
-  })
-})
+test("the data is peanut butter", () => {
+  return fetchData().then((data) => {
+    expect(data).toBe("peanut butter");
+  });
+});
