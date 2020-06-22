68c6b7b57e39bc0c34a427ad65bde2bc2ff3086d
Ritter99 <39842670+Ritter99@users.noreply.github.com>
2020-06-22 10:49:15 -0400

    testing pr (#2)
    * testing pr
    
    * Correcting errors for Jest
    
    Co-authored-by: Isaiah Ritter <isaiah@casted.us>

--- tests/random.test.js
+++ tests/random.test.js
@@ -1,20 +1,19 @@
-test('object falsy', () => {
-    const data = {
-      one: 1
-    };
-    expect(data).toEqual({
-      one: 1,
-      two: 2
-    });
-  })
-  test('object contain', () => {
-    const data = {
-      one: 1,
-      two: 2,
-      three: 4,
-    };
-    expect(data).toContain({
-      one: 1,
-      two: 2
-    });
-  })
+test("object falsy", () => {
+  const data = {
+    one: 1,
+  };
+  expect(data).toEqual({
+    one: 1,
+  });
+});
+test("object contain", () => {
+  const data = {
+    one: 1,
+    two: 2,
+    three: 3,
+  };
+  expect(data).toMatchObject({
+    one: 1,
+    two: 2,
+  });
+});
